bde994d78693b16dc322b3e4145a1b9aad0ddbc7
Training_C2a.05.01 <Training_C2a.05.01@CDC2-D-CJ3MXJ2.dir.svc.accenture.com>
2019-04-01 11:09:43 +0530

    commit2

--- src/main/java/Day2/Day2Demo/App.java
+++ src/main/java/Day2/Day2Demo/App.java
@@ -1,13 +1,21 @@
 package Day2.Day2Demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+		System.setProperty("webdriver.chrome.driver", "C:/Selenium/chromdriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
     }
 }
